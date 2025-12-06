c7e35ebe1ef6624b34d49b15cc114f13520026ed
ball_lin <gillharsher1@gmail.com>
2025-12-06 19:46:41 +0530

    Update agent.py
    accomodate dqn

--- agent.py
+++ agent.py
@@ -1,18 +1,31 @@
 import flappy_bird_gymnasium
 import gymnasium
-env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=False)
+from dqn import DQN
+import torch
 
-obs, _ = env.reset()
-while True:
-    # Next action:
-    # (feed the observation to your agent here)
-    action = env.action_space.sample()
+device = "mps" if torch.backends.mps.is_available() else "cpu"
 
-    # Processing:
-    obs, reward, terminated, _, info = env.step(action)
-    
-    # Checking if the player is still alive
-    if terminated:
-        break
+class Agent:
+    def run(self, is_training=True, render=False):
 
-env.close()
+        env = gymnasium.make("FlappyBird-v0", render_mode="human" if render else None)
+
+        input_parameters = env.observation_space.shape[0]
+        output_parameters = env.action_space.n
+
+        policy_net = DQN(input_parameters, output_parameters).to(device)
+
+        obs, _ = env.reset()
+        while True:
+            # Next action:
+            # (feed the observation to your agent here)
+            action = env.action_space.sample()
+
+            # Processing:
+            obs, reward, terminated, _, info = env.step(action)
+            
+            # Checking if the player is still alive
+            if terminated:
+                break
+
+        env.close()
